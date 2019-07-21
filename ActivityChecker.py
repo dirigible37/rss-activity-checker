45750196785a936569a1da4fd187f71cc9bc2f20
dirigible37 <mohw011@gmail.com>
2019-07-21 15:52:30 -0400

    Initial activity checker and xml parser functionality. Added requirements.txt for pip

---
+++ ActivityChecker.py
@@ -0,0 +1,38 @@
+import numpy as np
+import requests
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup as bs
+
+
+class ActivityChecker(object):
+    
+    def activitySince(self, num_days, company_dict):
+        assert num_days > 0
+        date_since = datetime.today() - timedelta(days=num_days)
+        
+        inactive_companies = []
+
+        for company in company_dict:
+            rss_feeds = company_dict[company]
+            last_active_date = self.findLastActivity(rss_feeds)
+            if(last_active_date < date_since):
+                inactive_companies.append(company)
+        
+        return inactive_companies
+
+    def findLastActivity(self, feeds):
+        last_active_date = None
+        for feed in feeds:
+            response = requests.get(feed).text
+            soup = bs(response,'xml')
+            for pubdate in soup.find_all('pubDate'):
+                print(pubdate.text)
+        
+        return last_active_date
+
+test_data = {
+    "bill_maher": ["http://billmaher.hbo.libsynpro.com/rss"],
+    "bill_simmons": ["https://rss.art19.com/the-bill-simmons-podcast"]
+}
+checker = ActivityChecker()
+inactive = checker.activitySince(0, test_data)
