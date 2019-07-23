be5c823ffa57df5e344bd512e13f6aaf60e9b352
Winslow Mohr <mohw011@gmail.com>
2019-07-23 09:11:17 -0400

    Made default logging setup as "stdout"

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from Logger import Logger
 from Tester import Tester
 
 if __name__ == "__main__":
-  logger = Logger('file')
+  logger = Logger('stdout')
   checker = ActivityChecker(logger)
 
   test_data = {
